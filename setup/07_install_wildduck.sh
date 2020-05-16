7a32e59a62cd0f955463453ee5042b3856abd11d
Jelle De Loecker <jelle@develry.be>
2020-05-16 21:23:20 +0200

    Use our "skerit" fork in the install command

--- setup/07_install_wildduck.sh
+++ setup/07_install_wildduck.sh
@@ -19,7 +19,7 @@ rm -rf /etc/wildduck
 
 # fresh install
 cd /var/opt
-git clone --bare git://github.com/nodemailer/wildduck.git
+git clone --bare git://github.com/skerit/wildduck.git
 
 # create update hook so we can later deploy to this location
 hook_script wildduck
